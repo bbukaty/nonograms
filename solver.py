f9f5bea6842f84bf037a734d1b480481fe37b624
Buck Bukaty <buck.bukaty@gmail.com>
2023-09-05 11:21:56 -0700

    it works!

--- solver.py
+++ solver.py
@@ -7,7 +7,8 @@ class Tile:
 
     def __init__(self):
         self.status = " "
-        self.isFresh = False
+        self.row = None
+        self.col = None
 
     def __str__(self):
         return self.status
@@ -22,7 +23,9 @@ class Line(Sequence):
     We try to narrow these until they are the length of the block itself.
     """
 
-    def __init__(self, tiles, blocks):
+    def __init__(self, orientation, position, tiles, blocks):
+        self.orientation = orientation  # 'row' or 'col'.
+        self.position = position
         self.tiles = tiles
         self.blocks = blocks
         self.blockDomains = []
@@ -33,8 +36,13 @@ class Line(Sequence):
     def __len__(self):
         return len(self.tiles)
 
-    def __str__(self) -> str:
-        return "".join([str(tile) for tile in self.tiles])
+    def __contains__(self, value):
+        for tile in self:
+            if tile.status == value: return True
+        return False
+
+    def __str__(self):
+        return f"{self.orientation} {self.position}"
 
     def fillXs(self):
         for tile in self:
@@ -52,6 +60,28 @@ def printPuzzle(puzzle):
     print("│" + "│".join(puzzle[-1]) + "│")
     print("└" + "─┴" * (width-1) + "─┘")
 
+
+def setUpTileRefs(height, width, rowHints, colHints):
+    """Returns a few different useful ways of indexing into/around the puzzle grid."""
+    rows = []
+    for rowIndex in range(height):
+        rowTiles = [Tile() for _ in range(width)]
+        rowBlocks = rowHints[rowIndex]
+        rows.append(Line("row", rowIndex, rowTiles, rowBlocks))
+    cols = []
+    for colIndex in range(width):
+        colTiles = [row[colIndex] for row in rows]
+        colBlocks = colHints[colIndex]
+        cols.append(Line("col", colIndex, colTiles, colBlocks))
+
+    for rowIndex in range(height):
+        for colIndex in range(width):
+            tile = rows[rowIndex][colIndex]
+            tile.row = rows[rowIndex]
+            tile.col = cols[colIndex]
+    return (rows, cols, rows + cols)
+
+
 def initBlockDomains(line):
     """Block by block, "slides" all other blocks out of the way to determine
     the full area that block could be placed in.
@@ -66,34 +96,16 @@ def initBlockDomains(line):
         line.blockDomains.append((spaceBefore, len(line) - spaceAfter - 1))
         spaceBefore += block + 1
 
-
 def fillDomainCenters(line):
-    """Fills middle of block domains based on simple "sliding" heuristic."""
-    if not line.blockDomains: return
+    """Fills middle of domains where block is longer than half domain size."""
+    if not line.blockDomains: return []
 
-    for i in range(len(line.blocks)):
+    for i in range(len(line.blockDomains)):
         blockLen, blockDomain = line.blocks[i], line.blockDomains[i]
         domainStart, domainEnd = blockDomain
         domainLen = domainEnd - domainStart + 1
-        if 2 * blockLen > domainLen:
-            line.hasNewTiles = True
         for i in range(domainLen - blockLen, blockLen):
             line[domainStart + i].status = "O"
-
-
-def setUpTileRefs(height, width, rowHints, colHints):
-    """Returns a few different useful ways of indexing into the puzzle grid."""
-    rows = []
-    for row in range(height):
-        rowTiles = [Tile() for _ in range(width)]
-        rowBlocks = rowHints[row]
-        rows.append(Line(rowTiles, rowBlocks))
-    cols = []
-    for col in range(width):
-        colTiles = [row[col] for row in rows]
-        colBlocks = colHints[col]
-        cols.append(Line(colTiles, colBlocks))
-    return (rows, cols, rows + cols)
 
 
 def getActiveDomains(line, i):
@@ -101,7 +113,7 @@ def getActiveDomains(line, i):
     activeDomains = []
     for domainIndex, domain in enumerate(line.blockDomains):
         a, b = domain
-        if a <= i or i <= b:
+        if a <= i and i <= b:
             activeDomains.append(domainIndex)
     return activeDomains
 
@@ -110,7 +122,7 @@ def anchorDomainsAroundOs(line):
     for i, tile in enumerate(line):
         if tile.status != "O":
             continue
-        activeDomains = line.getActiveDomains()
+        activeDomains = getActiveDomains(line, i)
 
         assert len(activeDomains) > 0  # every O should have >=1 domains
         if len(activeDomains) == 1:
@@ -120,9 +132,28 @@ def anchorDomainsAroundOs(line):
             a, b = currDomain
             newA = max(i - blockLen + 1, a)
             newB = min(i + blockLen - 1, b)
-            print(f"updating blockDomain from ({a},{b}) ({newA},{newB})")
             line.blockDomains[blockIndex] = (newA, newB)
 
+def fillNoDomainsWithXs(line):
+    """Sweep a line for regions with no active domains. Fill with Xs."""
+    for i, tile in enumerate(line):
+        if tile.status != " ":
+            continue
+        activeDomains = getActiveDomains(line, i)
+
+        if not activeDomains:
+            tile.status = "X"
+
+def constrainDomainsWithinXs(line):
+    for i in range(len(line.blockDomains)):
+        blockLen, blockDomain = line.blocks[i], line.blockDomains[i]
+        domainStart, domainEnd = blockDomain
+        while "X" in [tile.status for tile in line[domainStart:domainStart+blockLen]]:
+            domainStart += 1
+        while "X" in [tile.status for tile in line[domainEnd-blockLen+1:domainEnd+1]]:
+            domainEnd -= 1
+        line.blockDomains[i] = (domainStart, domainEnd)
+        # while "X" in line
 
 def solvePuzzle(height, width, rowHints, colHints):
     assert len(rowHints) == height and len(colHints) == width
@@ -131,7 +162,18 @@ def solvePuzzle(height, width, rowHints, colHints):
     for line in lines:
         initBlockDomains(line)
         fillDomainCenters(line)
-    
+
+    for i in range(2):
+        for line in lines:
+            anchorDomainsAroundOs(line)
+        for line in lines:
+            pass
+        for line in lines:
+            fillNoDomainsWithXs(line)
+            constrainDomainsWithinXs(line)
+        for line in lines:
+            fillDomainCenters(line)
+
     return [[str(tile) for tile in row] for row in rows]
 
 
